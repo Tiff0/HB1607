3a9e25b57afe296253edce9b40456c047e1290ff
Zhenya <zTiff0@gmail.com>
2018-07-16 01:33:03 +0300

    Happy Birthday App

--- HB1607/Program.cs
+++ HB1607/Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using System.Drawing;
+using static System.Console;
+using static System.Threading.Thread;
 
 namespace HB1607
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private const int Red = 244;
+        private const int Green = 212;
+        private const int Blue = 255;
+        private const int DotCount = 100;
+        private const int SleepTime = 40;
+
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DrawLine("Loading", Red, Green, Blue);
+            DrawDots();
+            DrawLine("Date: " + DateTime.Now.Date.ToShortDateString(), Red, Green, Blue);
+            DrawLine("Name: Philip", Red, Green, Blue);
+            DrawDots();
+            Clear();
+            DrawLine("Happy Birthday Philip!", Red, Green, Blue);
+            ReadKey(true);
+        }
+
+        private static void DrawDots()
+        {
+            for (var i = 0; i < DotCount; i++)
+            {
+                Write(".");
+                Sleep(SleepTime);
+
+                if (i == DotCount - 1)
+                {
+                    WriteLine(Environment.NewLine);
+                }
+            }
+        }
+
+        private static void DrawLine(string line, int da, int v, int id)
+        {
+            Colorful.Console.WriteAscii(line, Color.FromArgb(da, v, id));
+            Sleep(SleepTime);
         }
     }
 }
