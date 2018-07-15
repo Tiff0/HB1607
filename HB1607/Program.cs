7e406694790cbb20fcd8d5f8404bd4a11faa80d5
Zhenya <zTiff0@gmail.com>
2018-07-15 12:10:45 +0300

    Add project files.

---
+++ HB1607/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HB1607
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
